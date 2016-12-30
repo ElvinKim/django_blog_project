b455afdcede49281fefdcfd3798fbf21e2d0b942
ElvinKim <bonwho09@gmail.com>
2016-12-31 02:43:16 +0900

    [edit] add CRUD

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,7 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def post_home(request):
-    return HttpResponse("<h1>Hello World!!!</h1>")
+def post_create(request):
+    return HttpResponse("<h1>CREATE</h1>")
 
+
+def post_detail(request):
+    return HttpResponse("<h1>DETAIL</h1>")
+
+
+def post_list(request):
+    return render(request, "index.html", {})
+
+
+def post_update(request):
+    return HttpResponse("<h1>UPDATE</h1>")
+
+
+def post_delete(request):
+    return HttpResponse("<h1>DELETE</h1>")
 
