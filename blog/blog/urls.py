238f4d0a1fd3eff309e884d265c95d7b81544764
ElvinKim <bonwho09@gmail.com>
2017-01-01 14:20:18 +0900

    [edit] URL Links & Get Absolute URL

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from posts import views as post_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^posts/$', post_views.post_home),
+    url(r'^posts/', include("posts.urls", namespace="posts")),
 ]
