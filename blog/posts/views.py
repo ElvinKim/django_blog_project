3f59a26f80109852daa373a68c20bb33945fab20
ElvinKim <bonwho09@gmail.com>
2017-01-03 01:37:40 +0900

    [edit] add form to post_create

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,10 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from posts.models import Post
+from .forms import PostForm
 
 
 def post_create(request):
-    return HttpResponse("<h1>CREATE</h1>")
+    form = PostForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "post_form.html", context)
 
 
 def post_detail(request, id=None):
