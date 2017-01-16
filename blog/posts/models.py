45e5551b346464ea1563f87d97f92f895f737aa7
ElvinKim <bonwho09@gmail.com>
2017-01-16 20:56:13 +0900

    [edit] add user field

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
+from django.conf import settings
 
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(blank=True,
